1d77baf114dc8edfcad39406a256767a871f1874
tskdsb <tsk912@qq.com>
2020-03-14 23:04:05 +0800

    feat: main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+)
+
+func main() {
+	var (
+		addr string
+		dir  string
+	)
+
+	flag.StringVar(&addr, "addr", "0.0.0.0:4500", "ip:port")
+	flag.StringVar(&dir, "dir", ".", "directory to export")
+
+	panic(http.ListenAndServe(addr, http.FileServer(http.Dir(dir))))
+}
